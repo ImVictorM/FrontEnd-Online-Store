1ef3a755cd7232cb1ed49c1825a724e607cd7af7
rromulo <rromulo.silva21@gmail.com>
2022-09-08 18:24:44 -0300

    Adiciona Component Cart e conclui requisito 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './Component/Home';
+import Cart from './Component/Cart';
 // import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
 class App extends Component {
@@ -9,7 +10,8 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ Home } />
+          <Route exact path="/" component={ Home } />
+          <Route path="/Cart" component={ Cart } />
         </Switch>
       </BrowserRouter>
     );
