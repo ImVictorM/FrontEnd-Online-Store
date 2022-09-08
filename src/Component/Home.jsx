292a6454eeb503a26f06aa38db37a9eff375ca54
Victor Mendes <victorutdsim7@gmail.com>
2022-09-08 19:53:42 -0300

    merge requisito 4 e 5

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends React.Component {
   state = {
+    searchField: '',
+    products: [],
+    searchPerformed: false,
     categorie: [],
   };
 
@@ -12,8 +15,25 @@ class Home extends React.Component {
     this.setState({ categorie: categories });
   }
 
+  changeHandle = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  searchByQuery = async () => {
+    const { searchField } = this.state;
+    const response = await getProductsFromCategoryAndQuery(null, searchField);
+    const products = response.results;
+    this.setState({
+      products: [...products],
+      searchPerformed: true,
+    });
+  };
+
   render() {
-    const { categorie } = this.state;
+    const { products, searchPerformed, categorie } = this.state;
     return (
       <div>
         <section>
@@ -28,9 +48,47 @@ class Home extends React.Component {
           </ul>
         </section>
         <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou esculha uma categoria.
+          Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
         <Link data-testid="shopping-cart-button" to="/Cart">Cart</Link>
+        <input
+          name="searchField"
+          type="text"
+          data-testid="query-input"
+          onChange={ this.changeHandle }
+        />
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.searchByQuery }
+        >
+          Buscar
+        </button>
+
+        {
+          searchPerformed && (
+            <section>
+              {
+                products.length !== 0
+                  ? (
+                    products.map((item) => {
+                      const { id, title, price, thumbnail } = item;
+                      return (
+                        <div key={ id } data-testid="product">
+                          <p>{title}</p>
+                          <img src={ thumbnail } alt="produto" />
+                          <p>{price}</p>
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <p>Nenhum produto foi encontrado</p>
+                  )
+              }
+            </section>
+          )
+        }
+
       </div>
     );
   }
